5a35a2a982e24a6af90a2c0c4a24e408697a1bd4
Miloš Kozák <lejmr@users.noreply.github.com>
2020-11-27 10:23:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,14 +12,37 @@ server {
     }
 
     location / {
-       add_header 'Access-Control-Allow-Origin' '${PROXY_ALLOW_ORIGIN:-*}';
-       add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
-       add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
-       add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range';
+         
+         # CORS
+         if ($request_method = 'OPTIONS') {
+           add_header 'Access-Control-Allow-Origin' "${PROXY_ALLOW_ORIGIN:-*}";
+           add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+           add_header 'Access-Control-Allow-Headers' 'Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Mx-ReqToken,X-Requested-With';
+           add_header 'Access-Control-Allow-Credentials' 'true';
+           add_header 'Access-Control-Max-Age' 1728000;
+           add_header 'Content-Type' 'text/plain; charset=utf-8';
+           add_header 'Content-Length' 0;
+           return 204;
+        }
+        if ($request_method = 'POST') {
+           add_header 'Access-Control-Allow-Origin' "${PROXY_ALLOW_ORIGIN:-*}";
+           add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+           add_header 'Access-Control-Allow-Headers' 'Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Mx-ReqToken,X-Requested-With';
+           add_header 'Access-Control-Allow-Credentials' 'true';
+           add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range';
+        }
+        if ($request_method = 'GET') {
+           add_header 'Access-Control-Allow-Origin' "${PROXY_ALLOW_ORIGIN:-*}";
+           add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+           add_header 'Access-Control-Allow-Headers' 'Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Mx-ReqToken,X-Requested-With';
+           add_header 'Access-Control-Allow-Credentials' 'true';
+           add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range';
+        }
        
+       # Proxy path
        proxy_pass ${PROXY_PROTO:-https}://${PROXY_ADDRESS}; 
        proxy_set_header Authorization "Bearer ${PROXY_BEARER}";
-       add_header 'Access-Control-Allow-Origin' '*';
+       # add_header 'Access-Control-Allow-Origin' '*';
     }
 }
 EOF
