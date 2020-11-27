28618fe1d931840eed1add76f1239f9da1c38993
Miloš Kozák <lejmr@users.noreply.github.com>
2020-11-27 10:00:05 +0100

    Adds externalization for Allow origin option

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,11 @@ server {
     }
 
     location / {
+       add_header 'Access-Control-Allow-Origin' '${PROXY_ALLOW_ORIGIN:-*}';
+       add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+       add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
+       add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range';
+       
        proxy_pass ${PROXY_PROTO:-https}://${PROXY_ADDRESS}; 
        proxy_set_header Authorization "Bearer ${PROXY_BEARER}";
        add_header 'Access-Control-Allow-Origin' '*';
